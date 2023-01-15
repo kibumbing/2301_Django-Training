21bc3207e7df3674c104cb4f9747ce6f4149744d
Kibum Bing <110390520+kibumbing@users.noreply.github.com>
2023-01-15 22:39:52 +0900

    Add files via upload

---
+++ django_rest_api_tutorial/addresses/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Address
+from .serializers import AddressSerializer
+from rest_framework.parsers import JSONParser
+
+# Create your views here.
+
+@csrf_exempt
+def address_list(request):
+    if request.method == 'GET':
+        query_set = Address.objects.all()
+        serializer = AddressSerializer(query_set, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AddressSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def address(request, pk):
+
+    obj = Address.objects.get(pk=pk)
+
+    if request.method == 'GET':
+        serializer = AddressSerializer(obj)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = AddressSerializer(obj, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        obj.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        search_name = data['name']
+        obj = Address.objects.get(name=search_name)
+
+        if data['phone_number'] == obj.phone_number:
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=400)
+
